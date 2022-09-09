d743e9ebacdbe02d74130fe9f8c27a9ec9e8cbde
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-09-09 15:15:21 -0500

    [ADD] useform custom hook

--- src/02-useEffect/FormWithCustomHook.jsx
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -1,5 +1,45 @@
+import { useForm } from '../hooks/useForm'
+
 export const FormWithCustomHook = () => {
+  const [formValues, onInputChange, onResetForm] = useForm({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  const { username, email, password } = formValues
+
   return (
-    <h3>FormWithCustomHook</h3>
+    <>
+      <h1>Form With Custom hook</h1>
+      <hr />
+
+      <input
+        type="text"
+        placeholder="username"
+        name="username"
+        value={username}
+        onChange={onInputChange}
+        autoFocus
+      />
+
+      <input
+        type="email"
+        placeholder="jhon@mail.com"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+
+      <input
+        type="password"
+        placeholder="********"
+        name="password"
+        value={password}
+        onChange={onInputChange}
+      />
+
+      <button onClick={onResetForm}>Reset</button>
+    </>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { CounterApp } from './01-useState/CounterApp'
 import { CounterWithHook } from './01-useState/CounterWithHook'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 import { SimpleForm } from './02-useEffect/SimpleForm'
 
 export const App = () => {
@@ -8,7 +9,8 @@ export const App = () => {
       <h1>HooksApp</h1>
       {/* <CounterApp /> */}
       {/* <CounterWithHook /> */}
-      <SimpleForm />
+      {/* <SimpleForm /> */}
+      <FormWithCustomHook />
     </>
   )
 }
